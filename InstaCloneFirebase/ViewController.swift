403994fff4a93fef46de86b4972c02eba31e7daf
Zeynep Bayrak Demirel <zeynepbayrak863@gmail.com>
2023-04-18 01:18:17 +0300

    firebase bağlantısı yapıldı, tab bar kontroller oluşturuldu, kullanıcı ara yüzü tasarlandı

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClıcked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
 }
 
+    
