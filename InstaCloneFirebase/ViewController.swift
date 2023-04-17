70b14ff683224a7714be06173523524c13cd26a1
Zeynep Bayrak Demirel <zeynepbayrak863@gmail.com>
2023-04-17 22:32:11 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Zeynep Bayrak Demirel on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
